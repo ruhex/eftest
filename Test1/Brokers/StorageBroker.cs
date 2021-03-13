b9962c17ed77c0a9ac7afa3e328a557b1a53e6e0
Mikhail Movchan <m.movchan@radek-lab.ru>
2021-03-13 13:54:04 +0300

    Add project files.

---
+++ Test1/Brokers/StorageBroker.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test1.Brokers
+{
+    public class StorageBroker : DbContext
+    {
+        public StorageBroker(DbContextOptions<StorageBroker> options)
+            : base(options)
+        {
+            //this.Database.Migrate();
+            // Database.EnsureDeleted();
+            //Database.EnsureCreated();
+        }
+    }
+}
---
+++ Test1/Controllers/Test.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Test1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class Test : ControllerBase
+    {
+
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new List<string>();
+        }
+    }
+}
