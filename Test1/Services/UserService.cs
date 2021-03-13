cdcee77cdb044b7bde63b0efa45e225800a62368
Mikhail Movchan <m.movchan@radek-lab.ru>
2021-03-13 14:10:21 +0300

    implementation IUserService

---
+++ Test1/Services/UserService.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Test1.Brokers;
+using Test1.Models;
+
+namespace Test1.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly StorageBroker _context;
+        public UserService(StorageBroker context) => _context = context;
+
+        public async Task<User> CreateAsync(User user)
+        {
+            await _context.AddAsync(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<IEnumerable<User>> GetAllAsync()
+        {
+            return await _context.Users.Where(x => true).ToArrayAsync();
+        }
+
+        public async Task<User> GetByIdAsync(int id)
+        {
+            return await _context.Users.FirstAsync(x => x.Id == id);
+        }
+    }
+
+    public interface IUserService
+    {
+        Task<User> CreateAsync(User user);
+        Task<User> GetByIdAsync(int id);
+        Task<IEnumerable<User>> GetAllAsync();
+    }
+}
